ca8b1024039e5394d620e7387743920af9b7a39b
Reezyx <58127932+Reezyx@users.noreply.github.com>
2022-07-16 06:37:27 +0000

    Deploying to gh-pages from @ Reezyx/portofolio@f58843b0f310d04417414557035cbbcbf0dcc3c0 🚀

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-// $('.page-scroll').on('click', function (e) {
+const home = document.querySelector('.home');
+const skills = document.querySelector('.skills');
+const experience = document.querySelector('.experience');
+const project = document.querySelector('.project');
 
-//   var tujuan = $(this).attr('href');
-
-//   var elemenTujuan = $(tujuan);
-
-//   $('html , body').animate({
-//     scrollTop: elemenTujuan.offset().top - 1000
-//   });
-
-//   e.preventDefault();
-// });
+function scrolltoHome(){
+  document.querySelector('#home').scrollIntoView({
+    behavior: 'smooth',
+  });
+  skills.classList.remove('nav-active');
+  experience.classList.remove('nav-active');
+  project.classList.remove('nav-active');
+  home.classList.add('nav-active');
+}
+function scrolltoSkill(){
+  document.querySelector('#skills').scrollIntoView({
+    behavior: 'smooth',
+  });
+  home.classList.remove('nav-active');
+  experience.classList.remove('nav-active');
+  project.classList.remove('nav-active');
+  skills.classList.add('nav-active');
+}
+function scrolltoExperience(){
+  document.querySelector('#experience').scrollIntoView({
+    behavior: 'smooth',
+  });
+  skills.classList.remove('nav-active');
+  home.classList.remove('nav-active');
+  project.classList.remove('nav-active');
+  experience.classList.add('nav-active');
+}
+function scrolltoProject(){
+  document.querySelector('#project').scrollIntoView({
+    behavior: 'smooth',
+  });
+  skills.classList.remove('nav-active');
+  experience.classList.remove('nav-active');
+  home.classList.remove('nav-active');
+  project.classList.add('nav-active');
+}
