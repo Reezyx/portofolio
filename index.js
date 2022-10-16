e93d201da41e6e645b7351d17c99b1299e8be83f
Reezyx <58127932+Reezyx@users.noreply.github.com>
2022-10-16 12:35:29 +0000

    Deploying to gh-pages from @ Reezyx/portofolio@ae4e46c43d3391eb5a0d960f242f751a778dfb8f 🚀

--- index.js
+++ index.js
@@ -2,98 +2,108 @@ const home = document.querySelector('.home');
 const skills = document.querySelector('.skills');
 const experience = document.querySelector('.experience');
 const project = document.querySelector('.project');
-
-// function scrolltoHome(){
-//   document.querySelector('#home').scrollIntoView({
-//     behavior: 'smooth',
-//   });
-//   skills.classList.remove('nav-active');
-//   experience.classList.remove('nav-active');
-//   project.classList.remove('nav-active');
-//   home.classList.add('nav-active');
-// }
-// function scrolltoSkill(){
-//   document.querySelector('#skills').scrollIntoView({
-//     behavior: 'smooth',
-//   });
-//   home.classList.remove('nav-active');
-//   experience.classList.remove('nav-active');
-//   project.classList.remove('nav-active');
-//   skills.classList.add('nav-active');
-// }
-// function scrolltoExperience(){
-//   document.querySelector('#experience').scrollIntoView({
-//     behavior: 'smooth',
-//   });
-//   skills.classList.remove('nav-active');
-//   home.classList.remove('nav-active');
-//   project.classList.remove('nav-active');
-//   experience.classList.add('nav-active');
-// }
-// function scrolltoProject(){
-//   document.querySelector('#project').scrollIntoView({
-//     behavior: 'smooth',
-//   });
-//   skills.classList.remove('nav-active');
-//   experience.classList.remove('nav-active');
-//   home.classList.remove('nav-active');
-//   project.classList.add('nav-active');
-// }
-
 const mediaDeviceWidth = window.screen.width;
-window.onscroll = () => {
-  // if(mediaDeviceWidth <= 800){
-  //   if (window.scrollY >= 600 && window.scrollY < 1200) {
-  //     console.log('ok');
-  //   project.classList.remove('nav-active');
-  //   experience.classList.remove('nav-active');
-  //   home.classList.remove('nav-active');
-  //   skills.classList.add('nav-active');
-  //   }
-  //   if (window.scrollY >= 2000 && window.scrollY < 1800){
-  //     project.classList.remove('nav-active');
-  //     skills.classList.remove('nav-active');
-  //     home.classList.remove('nav-active');
-  //     experience.classList.add('nav-active');
-  //   }
-  //   if (window.scrollY >= 1800){
-  //     skills.classList.remove('nav-active');
-  //     experience.classList.remove('nav-active');
-  //     home.classList.remove('nav-active');
-  //     project.classList.add('nav-active');
-  //   }
-  //   if (window.scrollY >= 0 && window.scrollY < 600){
-  //     skills.classList.remove('nav-active');
-  //     experience.classList.remove('nav-active');
-  //     project.classList.remove('nav-active');
-  //     home.classList.add('nav-active');
-  //   }
+const mediaMobile = window.matchMedia("(max-width: 720px)");
+// const mediaTablet = window.matchMedia("(max-width: 1112px)");
+const mediaLaptop = window.matchMedia("(max-width: 1920px)");
+
+$(document).ready(function() {
+  if (mediaMobile.matches){
+    mediaMobileScroll();
+  }  else if (mediaLaptop.matches){
+    mediaLaptopScroll();
+  }
+  // else if (mediaTablet.matches){
+  //   mediaTabletScroll();
   // }
-  // if(mediaDeviceWidth >= 1280){
-    if (window.scrollY >= 600 && window.scrollY < 1200) {
-      // console.log('ok');
-      project.classList.remove('nav-active');
-      experience.classList.remove('nav-active');
-      home.classList.remove('nav-active');
-      skills.classList.add('nav-active');
-    }
-    if (window.scrollY >= 1200 && window.scrollY < 1800){
-      project.classList.remove('nav-active');
-      skills.classList.remove('nav-active');
-      home.classList.remove('nav-active');
-      experience.classList.add('nav-active');
-    }
-    if (window.scrollY >= 1750){
-      skills.classList.remove('nav-active');
-      experience.classList.remove('nav-active');
-      home.classList.remove('nav-active');
-      project.classList.add('nav-active');
-    }
-    if (window.scrollY >= 0 && window.scrollY < 600){
-      skills.classList.remove('nav-active');
-      experience.classList.remove('nav-active');
-      project.classList.remove('nav-active');
-      home.classList.add('nav-active');
-    }
-  // }
+});
+
+function mediaMobileScroll(){
+  console.log("mediaMobile");
+  window.onscroll = () => {
+      if (window.scrollY >= 1080 && window.scrollY < 2800) {
+        project.classList.remove('nav-active');
+        experience.classList.remove('nav-active');
+        home.classList.remove('nav-active');
+        skills.classList.add('nav-active');
+      }
+      if (window.scrollY >= 2800 && window.scrollY < 4550){
+        project.classList.remove('nav-active');
+        skills.classList.remove('nav-active');
+        home.classList.remove('nav-active');
+        experience.classList.add('nav-active');
+      }
+      if (window.scrollY >= 4550){
+        skills.classList.remove('nav-active');
+        experience.classList.remove('nav-active');
+        home.classList.remove('nav-active');
+        project.classList.add('nav-active');
+      }
+      if (window.scrollY >= 0 && window.scrollY < 1080){
+        skills.classList.remove('nav-active');
+        experience.classList.remove('nav-active');
+        project.classList.remove('nav-active');
+        home.classList.add('nav-active');
+      }
+  }
+}
+
+// function mediaTabletScroll(){
+//   console.log('mediaTablet');
+//   window.onscroll = () => {
+//       if (window.scrollY >= 600 && window.scrollY < 1200) {
+//         project.classList.remove('nav-active');
+//         experience.classList.remove('nav-active');
+//         home.classList.remove('nav-active');
+//         skills.classList.add('nav-active');
+//       }
+//       if (window.scrollY >= 1200 && window.scrollY < 1800){
+//         project.classList.remove('nav-active');
+//         skills.classList.remove('nav-active');
+//         home.classList.remove('nav-active');
+//         experience.classList.add('nav-active');
+//       }
+//       if (window.scrollY >= 1750){
+//         skills.classList.remove('nav-active');
+//         experience.classList.remove('nav-active');
+//         home.classList.remove('nav-active');
+//         project.classList.add('nav-active');
+//       }
+//       if (window.scrollY >= 0 && window.scrollY < 600){
+//         skills.classList.remove('nav-active');
+//         experience.classList.remove('nav-active');
+//         project.classList.remove('nav-active');
+//         home.classList.add('nav-active');
+//       }
+//   }
+// }
+
+function mediaLaptopScroll(){
+  console.log('mediaLaptopScroll');
+  window.onscroll = () => {
+      if (window.scrollY >= 730 && window.scrollY < 1500) {
+        project.classList.remove('nav-active');
+        experience.classList.remove('nav-active');
+        home.classList.remove('nav-active');
+        skills.classList.add('nav-active');
+      }
+      if (window.scrollY >= 1500 && window.scrollY < 2240){
+        project.classList.remove('nav-active');
+        skills.classList.remove('nav-active');
+        home.classList.remove('nav-active');
+        experience.classList.add('nav-active');
+      }
+      if (window.scrollY >= 2240){
+        skills.classList.remove('nav-active');
+        experience.classList.remove('nav-active');
+        home.classList.remove('nav-active');
+        project.classList.add('nav-active');
+      }
+      if (window.scrollY >= 0 && window.scrollY < 730){
+        skills.classList.remove('nav-active');
+        experience.classList.remove('nav-active');
+        project.classList.remove('nav-active');
+        home.classList.add('nav-active');
+      }
+  }
 }
